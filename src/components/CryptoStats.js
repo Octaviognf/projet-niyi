ad375b369f3f9dc75e2cabbf94c025f615cc4c0b
Octaviognf <octaviognonlonfoun@gmail.com>
2025-03-10 18:32:53 +0100

    Update CryptoStats.js

--- src/components/CryptoStats.js
+++ src/components/CryptoStats.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement } from "chart.js";
 
-// ✅ Enregistrement des modules Chart.js
+//  Enregistrement des modules Chart.js
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement);
 
 const CryptoStats = ({ cryptoData }) => {
